05d00487fecdd1a1458b6cfbcbe04dcca899671a
Tobias Zwick <newton@westnordost.de>
2023-10-23 21:34:14 +0200

    update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "de.westnordost.luftlinie"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 6
         versionName = "1.5"
     }
@@ -41,22 +41,22 @@ dependencies {
     val kotlinxCoroutinesVersion = "1.7.1"
 
     // Kotlin
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinxCoroutinesVersion")
 
     // Android widgets
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
 
     implementation("de.westnordost:osmfeatures-android:5.2")
 
     // View models
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 
     // HTTP API
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
