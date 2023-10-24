75d18dae6a75003df2457382bc3f9779bb7cc3a8
Tobias Zwick <newton@westnordost.de>
2023-10-24 15:02:32 +0200

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "de.westnordost.luftlinie"
         minSdk = 21
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
     }
 
     buildTypes {
